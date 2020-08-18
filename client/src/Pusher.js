ffae7657063144d1eb116eefdd0ab128dd3d4e07
Marc Cordova <mcordova47@gmail.com>
2020-08-18 11:02:25 -0400

    Refactor

--- client/src/Pusher.js
+++ client/src/Pusher.js
@@ -2,7 +2,5 @@ const pusher = new Pusher('fd32b87508ffffee4257', { cluster: 'us2' })
 
 exports.subscribe_ = function (channelName, fn) {
   const channel = pusher.subscribe(channelName)
-  channel.bind('reacted', (data) => {
-    fn(data)()
-  })
+  channel.bind('reacted', data => fn(data)())
 }
